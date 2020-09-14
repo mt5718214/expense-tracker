840763c3ed33747fb87744e0da64456434a6d422
Lin <mt5718214@gmail.com>
2020-09-14 19:06:55 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoose error')
+})
+
+db.once('open', () => {
+  console.log('mongoose connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('This is Express app')
