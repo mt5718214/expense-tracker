5cff68781b5466fbd5cf77efebf9388be968caa1
Lin <mt5718214@gmail.com>
2020-11-11 21:15:38 +0800

    feat: add user authentication in search record & home routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const search = require('./modules/search')
 const record = require('./modules/record')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/search', search)
-router.use('/record', record)
-router.use('/', home)
+router.use('/search', authenticator, search)
+router.use('/record', authenticator, record)
+router.use('/', authenticator, home)
 
 module.exports = router
