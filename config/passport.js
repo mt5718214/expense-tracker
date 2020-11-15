5a018812a8b559b6c8ca6fc392d83db79aa4069c
Lin <mt5718214@gmail.com>
2020-11-15 11:30:30 +0800

    feat: add Login failed message

--- config/passport.js
+++ config/passport.js
@@ -12,10 +12,10 @@ module.exports = app => {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
       .then(user => {
-        if (!user) return done(null, false)
+        if (!user) return done(null, false, { message: '該信箱未註冊。' })
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false)
+            return done(null, false, { message: '信箱或密碼輸入錯誤。' })
           }
           return done(null, user)
         })
