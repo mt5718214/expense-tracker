3ad753180671027d24543a2bc991c7e4773794aa
Lin <mt5718214@gmail.com>
2021-02-26 21:19:51 +0800

    modify: add search bar in index page & modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -7,7 +7,7 @@ const { generateMonth } = require('../../config/monthData')
 //類別與日期篩選器(filter)
 router.get('/', async (req, res) => {
   const userId = req.user._id
-  const { queryCategory, startDate, endDate } = req.query
+  const { queryCategory, startDate, endDate, keyword } = req.query
   const filter = { userId, date: { $gte: `${startDate}`, $lte: `${endDate}` } }
   if (queryCategory) {
     filter.category = queryCategory
@@ -15,12 +15,17 @@ router.get('/', async (req, res) => {
 
   try {
     const categories = await Category.find().lean()
-    const records = await Record.find(filter).lean().sort({ date: 'desc' })
+    let records = await Record.find(filter).lean().sort({ date: 'desc' })
+
+    //計算總金額與將date格式轉換為字串
     let totalAmount = 0
     records.forEach(record => {
       totalAmount += record.amount
       record.date = record.date.toISOString().slice(0, 10)
     })
+
+    //篩選符合keyword的資料
+    records = records.filter(record => record.name.toLowerCase().includes(keyword.trim().toLowerCase()))
 
     return res.render('index', { records, totalAmount, categories, startDate, endDate })
   } catch (error) {
